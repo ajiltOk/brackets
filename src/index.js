c706eeca3e8ab7c89c175f64df8108b94e4adf52
ajiltOk <21summer12@gmail.com>
2023-02-16 22:36:18 +0300

    feat: add the solution of the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let result = '';
+  let arr = str.split('');
+  if(arr.length % 2 == 0) {
+    bracketsConfig.forEach(element => {
+      result += element.join('');
+    });
+    if(result == str) {
+      return true;
+    } return false;
+  } return false;
 }
